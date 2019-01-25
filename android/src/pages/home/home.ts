422e06ea078e7d5d15709150ba547c458f82838a
Dima <makushin.dima@gmail.com>
2019-01-25 10:30:27 +0200

    up: home and scan page --> insert scan function from home to scan page

--- android/src/pages/home/home.ts
+++ android/src/pages/home/home.ts
@@ -1,72 +1,46 @@
 import { Component 			} from '@angular/core';
 import { NavController 		} from 'ionic-angular';
-import { QrScan 			} from '../../providers/qr-scan/qr-scan';
-import { ToolBox 			} from '../../providers/qr-scan/ToolBox';
+
+import { ScanPage 			} from '../scan/scan';
 
 @Component({
 	selector: 		'page-home',
 	templateUrl: 	'home.html'
 })
 export class HomePage {
-	qrheader	: boolean = false;
-	textQR		: string;
-	show 		= null;
-	show2		: string = "show";
-	constructor(
-		public navCtrl		: NavController,
-		private qrscan		: QrScan) {
 
+	textQR			: string;
+	show 			= null;
+	servicesData 	: { data: string};
+
+	constructor(
+		public navCtrl		: NavController
+		) {
+			// if(this.navParams.data){
+			// 	this.servicesData = this.navParams.data;
+			// 	this.textQR = this.servicesData.data;
+			// }
 	}
 
 	ionViewDidLoad() {
-
+		console.log('this.navParams.data = ',this.navParams.data);
+		if(this.navParams.data){
+			this.servicesData = this.navParams.data;
+			this.textQR = this.servicesData.data;
+			//this.toggleAccordion('idx');
+		}
 	}
 
 	scan_qr_code() {
-		// this.toggleAccordion('idx');
+		this.toggleAccordion('idx');
+		this.navCtrl.push(ScanPage);
+		//---//
 
-		this.show2 = "hide";
-		console.log('this.show2 = ',this.show2);
-		let self = this;
-		this.qrheader = true;
-		console.log("Scan start");
-		Promise.resolve("proceed")
-			.then((k) => {
-				return this.qrscan.scan();
-			}).then((qrcode) => {
-				if (qrcode == "" || qrcode == undefined) {
-					return Promise.reject("invalid_qr_code");
-				}
-				self.qrheader = false;
-				this.textQR = qrcode;
-				//alert("Value：" + qrcode);
-				// return  your_method_with_passing_qr(qrcode);
-			}).then((data) => {
-				console.log(data);
-			}).catch((error) => {
-				if (error == "invalid_qr_code") {
-					console.log('your_custom_error_message');
-					this.textQR = error;
-				} else {
-					//handle error here.
-					this.textQR = "error";
-				}
-			});
-		this.show2 = 'show';
-		console.log('this.show2 = ',this.show2);
+
 		// this.toggleAccordion('idx');
 	}
 
-	exit_from_qr_scan(): void {
-		this.qrheader = false;
-		ToolBox.hideCamera();
-	}
-	change_camera(): void {
-		this.qrscan.chnage_camera();
-	}
-	light_enable_desable(): void {
-		this.qrscan.enaable_desable_light();
-	}
+
 
 	toggleAccordion(idx) {
 		if (this.isLevelShown(idx)) {
--- android/src/pages/scan/scan.ts
+++ android/src/pages/scan/scan.ts
@@ -2,21 +2,66 @@ import { Component 			} from '@angular/core';
 import { NavController		} from 'ionic-angular';
 import { NavParams 			} from 'ionic-angular';
 
+import { QrScan 			} from '../../providers/qr-scan/qr-scan';
+import { ToolBox 			} from '../../providers/qr-scan/ToolBox';
+
+import { HomePage 			} from '../home/home';
 
 @Component({
 	selector				: 'page-scan',
 	templateUrl				: 'scan.html',
 })
 export class ScanPage {
+	qrheader		: boolean = false;
 
 	constructor(
 		public navCtrl		: NavController,
+		private qrscan		: QrScan,
 		public navParams	: NavParams) {
-
+			let self = this;
+			this.qrheader = true;
+			console.log("Scan start");
+			Promise.resolve("proceed")
+				.then((k) => {
+					return this.qrscan.scan();
+				}).then((qrcode) => {
+					if (qrcode == "" || qrcode == undefined) {
+						return Promise.reject("invalid_qr_code");
+					}
+					self.qrheader = false;
+					this.navCtrl.pop(HomePage, {data: qrcode});
+					//this.textQR = qrcode;
+					//alert("Value：" + qrcode);
+					// return  your_method_with_passing_qr(qrcode);
+				}).then((data) => {
+					console.log(data);
+				}).catch((error) => {
+					if (error == "invalid_qr_code") {
+						console.log('your_custom_error_message');
+						this.navCtrl.pop(HomePage, {data: error});
+						//this.textQR = error;
+					} else {
+						//handle error here.
+						this.navCtrl.pop(HomePage, {data: 'handle error here!!!'});
+						// this.textQR = "error";
+					}
+				});
 	}
 
 	ionViewDidLoad() {
 		console.log('ionViewDidLoad ScanPage');
 	}
 
+	exit_from_qr_scan(): void {
+		this.qrheader = false;
+		ToolBox.hideCamera();
+	}
+
+	change_camera(): void {
+		this.qrscan.chnage_camera();
+	}
+
+	light_enable_desable(): void {
+		this.qrscan.enaable_desable_light();
+	}
 }
