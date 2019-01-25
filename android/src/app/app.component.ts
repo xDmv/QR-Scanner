ce8e836118e12e059002e26e0aa87db8446fc55d
Dima <makushin.dima@gmail.com>
2019-01-25 09:35:10 +0200

    up: style coding

--- android/src/app/app.component.ts
+++ android/src/app/app.component.ts
@@ -1,49 +1,54 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { Component 					} from '@angular/core';
+import { Platform 					} from 'ionic-angular';
+import { StatusBar 					} from '@ionic-native/status-bar';
+import { SplashScreen 				} from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
-import {QRScanner, QRScannerStatus} from "@ionic-native/qr-scanner";
+import { HomePage 					} from '../pages/home/home';
+
+import {QRScanner, QRScannerStatus	} from "@ionic-native/qr-scanner";
 
 @Component({
-  templateUrl: 'app.html'
+	templateUrl			: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+	rootPage:any = HomePage;
 
-  constructor( private qrScanner: QRScanner,platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      this.initQrcode();
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
+	constructor(
+		private qrScanner		: QRScanner,
+		platform				: Platform,
+		statusBar				: StatusBar,
+		splashScreen			: SplashScreen) {
+			this.StartApp();
+	}
 
-  initQrcode(){
-    // Optionally request the permission early
-    this.qrScanner.prepare()
-      .then((status: QRScannerStatus) => {
-        if (status.authorized) {
-          // camera permission was granted
+	StartApp(){
+		platform.ready().then(() => {
+			this.initQrcode();
+			statusBar.styleDefault();
+			splashScreen.hide();
+		});
+	}
 
-          alert("Wellcome!");
-
-        } else if (status.denied) {
-          alert("222");
-          // camera permission was permanently denied
-          // you must use QRScanner.openSettings() method to guide the user to the settings page
-          // then they can grant the permission from there
-        } else {
-          alert("333");
-          // permission was denied, but not permanently. You can ask for permission again at a later time.
-        }
-      })
-      .catch((e: any) => {
-        alert('Error is'+ e);
-        console.log('Error is', e);
-      });
-  }
+	initQrcode(){
+		// Optionally request the permission early
+		this.qrScanner.prepare().then((status: QRScannerStatus) => {
+			if (status.authorized) {
+				// camera permission was granted
+				alert("Wellcome!");
+				console.log('"Wellcome!"');
+			} else if (status.denied) {
+				console.log('"Wellcome!"');
+				alert("222");
+				// camera permission was permanently denied
+				// you must use QRScanner.openSettings() method to guide the user to the settings page
+				// then they can grant the permission from there
+			} else {
+				alert("333");
+				// permission was denied, but not permanently. You can ask for permission again at a later time.
+			}
+		}).catch((e: any) => {
+			alert('Error is'+ e);
+			console.log('Error is', e);
+		});
+	}
 }
